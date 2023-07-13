a0408c860b4415cd90b74d7535387fdd15e0efa7
All Kll <safdarian23@gmail.com>
2023-07-13 18:19:06 +0330

    Some widgets

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class FileButton(QLabel):
         self.setMaximumSize(40,40)
         self.setMinimumSize(40,40)
 class messagearea(QHBoxLayout):
-    def __init__(self,parent,flip = False):
+    def __init__(self,parent,text : str,flip = False):
         super().__init__()
         pho = QLabel(parent)
         pho.setMaximumSize(300,100)
@@ -39,7 +39,7 @@ class messagearea(QHBoxLayout):
             ph = ph.transformed(trans)
         pho.setPixmap(ph)
         messageText = QTextBrowser(pho)
-        messageText.setText("Hello World")
+        messageText.setText(text)
         messageText.setStyleSheet("background-color:rgba(0,0,0,0);color:white;"
                                   "padding-left:10px;padding-top:15px")
         self.addWidget(pho)
@@ -61,10 +61,7 @@ class leftSideWidget(QFrame):
         self.setMinimumSize(200,800)
         self.setMaximumWidth(300)
         self.setStyleSheet("border-radius:20px")
-        #leftSide.setMaximumSize(400,1080)
-        #grid = ContactsLayout()
-        #self.setLayout(grid)
-        #grid.setContentsMargins(4,0,0,0)
+
         self.setLayout(ContactsLayout(self))
 class TextFrameLayout(QHBoxLayout):
     def __init__(self,parent):
@@ -99,24 +96,24 @@ class MessagesFrame(QScrollArea):
         wid = QFrame()
         self.setStyleSheet("background-color:rgb(35, 35, 35)")
         self.setWidget(wid)
-        messagesList = MessagesList(self)
-        wid.setLayout(messagesList)
-    #def add_message(self,text):
+        self.messagesList = MessagesList(self)
+        wid.setLayout(self.messagesList)
+    def add_message(self,text):
+        self.messagesList.add_message_layout(text)
+
 
 
 class MessagesList(QGridLayout):
     def __init__(self,parent):
         super().__init__()
+        self.parent = parent
         self.setContentsMargins(0,0,0,5)
-        for i in range(0,16,4):          
-            self.addWidget(frame(parent),i,0,Qt.AlignmentFlag.AlignLeft)
-            self.addLayout(messagearea(parent,True),i+1,0,Qt.AlignmentFlag.AlignLeft)
-            self.addWidget(frame(parent),i+2,1,Qt.AlignmentFlag.AlignRight)
-            self.addLayout(messagearea(parent),i+3,1,Qt.AlignmentFlag.AlignRight)
+    def add_message_layout(self,text):
+        self.addWidget(frame(self.parent),self.rowCount() + 1,0,Qt.AlignmentFlag.AlignLeft)
+        self.addLayout(messagearea(self.parent,text,True),self.rowCount() + 1,0,Qt.AlignmentFlag.AlignLeft)
 class TextFrame(QFrame):
     def __init__(self,parent):
         super().__init__(parent)
-        #self.setStyleSheet("background-color:rgb(35, 35, 35)")
         self.setStyleSheet("background-color:none")
 class CentralWidget(QWidget):
     def __init__(self,parent):
@@ -149,7 +146,6 @@ class ContactsLayout(QVBoxLayout):
                                   "border-bottom:0px solid rgb(0, 31, 131);border-radius:10px")
             self.photo = frame(contact)
             contact.setMaximumHeight(100)
-            #self.photo.setGeometry(10,contact.height() / 2,contact.width() * 50 / 100,contact.width() * 50 / 100)
             ak.addWidget(self.photo,0,0)
             contact.setLayout(ak)
 
@@ -169,24 +165,14 @@ class ui(QMainWindow):
         rightSideLayout = RightSideWidgetLayout()
         self.resize(1000,700)
         self.setStyleSheet("background-color:rgba(46, 0, 182, 0.4)")
-        #
-        #leftSide.setStyleSheet("background-color:rgb(0, 4, 82)")
-        
-        #rightsidewidget.setGeometry(200,0,self.width()-200,self.height())
-        #rightsidewidget.setMaximumSize(1920,1080)
-        
-        #textFrame.setGeometry(0,rightsidewidget.height() - 50,rightsidewidget.width(),50)
+
         lay = TextFrameLayout(textFrame)
-        #lay.setSpacing(4)
-        #lay.setContentsMargins(5,5,5,5)
-        
-        
-        #lay.addLayout(we,Qt.AlignmentFlag.AlignLeft)
+
         textFrame.setLayout(lay)
-        #########################
         messagesFrame = MessagesFrame(rightsidewidget)
-        
-        #central.setLayout(mainlayout)
+        for i in range(10):
+            messagesFrame.add_message("hello")
+
         
         topSideWidgetProfileContact = QFrame(rightsidewidget)
         
